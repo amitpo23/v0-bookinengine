6a4b95fbaaf195d2682372d170edf2939b6ae0be
amitpo23 <159437337+amitpo23@users.noreply.github.com>
2025-12-23 19:04:49 +0200

    Add HotelResults component
    Initial implementation of HotelResults component.

---
+++ components/hotels/hotel-results.tsx
@@ -0,0 +1,6 @@
+// TODO: Copy from genspark_ai_developer
+// Run: git checkout genspark_ai_developer -- components/hotels/hotel-results.tsx
+
+export function HotelResults() {
+  return <div>Hotel Results</div>;
+}

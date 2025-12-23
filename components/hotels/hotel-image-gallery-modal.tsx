9827eeaa88cd03f8613ee4b7606539b0836a8d3f
amitpo23 <159437337+amitpo23@users.noreply.github.com>
2025-12-23 19:05:55 +0200

    Add HotelImageGalleryModal component

---
+++ components/hotels/hotel-image-gallery-modal.tsx
@@ -0,0 +1,6 @@
+// TODO: Copy from genspark_ai_developer
+// Run: git checkout genspark_ai_developer -- components/hotels/hotel-image-gallery-modal.tsx
+
+export function HotelImageGalleryModal() {
+  return <div>Hotel Image Gallery Modal</div>;
+}

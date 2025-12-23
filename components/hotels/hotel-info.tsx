7fd2880f1c3880691d59aebf1b690e6502658228
amitpo23 <159437337+amitpo23@users.noreply.github.com>
2025-12-23 19:04:03 +0200

    Add HotelInfo component
    Initial implementation of HotelInfo component.

---
+++ components/hotels/hotel-info.tsx
@@ -0,0 +1,6 @@
+// TODO: Copy from genspark_ai_developer
+// Run: git checkout genspark_ai_developer -- components/hotels/hotel-info.tsx
+
+export function HotelInfo() {
+  return <div>Hotel Info</div>;
+}

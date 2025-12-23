70217589e71a38bda5afa3c765a980b246b144d1
amitpo23 <159437337+amitpo23@users.noreply.github.com>
2025-12-23 19:05:22 +0200

    Add HotelImageGallery component

---
+++ components/hotels/hotel-image-gallery.tsx
@@ -0,0 +1,6 @@
+// TODO: Copy from genspark_ai_developer
+// Run: git checkout genspark_ai_developer -- components/hotels/hotel-image-gallery.tsx
+
+export function HotelImageGallery() {
+  return <div>Hotel Image Gallery</div>;
+}

a31a5cab0fdc68c8341c53b6dcbd5d84713f745d
amitpo23 <159437337+amitpo23@users.noreply.github.com>
2025-12-23 19:02:42 +0200

    Add HotelRating component for displaying hotel ratings

---
+++ components/hotels/hotel-rating.tsx
@@ -0,0 +1,6 @@
+// TODO: Copy from genspark_ai_developer
+// Run: git checkout genspark_ai_developer -- components/hotels/hotel-rating.tsx
+
+export function HotelRating() {
+  return <div>Hotel Rating</div>;
+}

1192435a703350b04bcf6b4cc7d921b63461ba7d
amitpo23 <159437337+amitpo23@users.noreply.github.com>
2025-12-23 19:00:18 +0200

    Create HotelCard component with placeholder content
    Initial implementation of HotelCard component.

---
+++ components/hotels/hotel-card.tsx
@@ -0,0 +1,6 @@
+// TODO: Copy full implementation from genspark_ai_developer branch
+// Run: git checkout genspark_ai_developer -- components/hotels/hotel-card.tsx
+
+export default function HotelCard() {
+  return <div>Hotel Card</div>;
+}
